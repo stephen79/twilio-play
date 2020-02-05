34b63bfcf212ddf084129a2c872d390d0d7bbae6
Stephen <stephen.adsl@gmail.com>
2020-02-05 15:59:14 -0800

    update index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 80 ; 
 
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(app.router);
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
