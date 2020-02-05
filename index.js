d557cddf054c0c6810589dae6ee903e25f1f44d2
Stephen <stephen.adsl@gmail.com>
2020-02-05 15:35:00 -0800

    update index

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80 ; 
 app.get('/', (req, res) => res.send('Hello World!'))
+// POST method route
+app.post('/makecall', function (req, res) {
+  console.log(`make call ${req}!`)
+  console.log(req.body);
+  res.send('POST request')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
