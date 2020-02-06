85ba63e20e839672e2863631ab54281918e6cff3
Stephen <stephen.adsl@gmail.com>
2020-02-05 16:05:04 -0800

    update index

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require('express');
-var bodyParser = require('body-parser')
-const app = express();
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 80 ; 
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
+const app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
-app.use(bodyParser.json())
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 // POST method route
 app.post('/makecall', function (req, res) {
+  const twiml = new VoiceResponse();
   if (req.body.To) {
+     const dial = twiml.dial({ callerId: '+13366001792' });
+     dial.number(req.body.To);
      console.log('make call:' + req.body.To);
   }
-  res.send('POST request')
+  else {
+     twiml.say('Thanks for calling!');
+  }
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml.toString());
 })
 
